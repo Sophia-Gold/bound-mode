653d66cae4d7329bc3ca4a21a0627d5a957835ac
Sophia Gold <sophiagoldnyc@gmail.com>
2018-05-21 00:29:00 -0400

    `fn->` macro and indexed variables

--- boundmode.el
+++ boundmode.el
@@ -1,18 +1,8 @@
-;; TO DO:
-;; params in different colors matching their occurrences in the body
-;; indexed literals in different colors
-;; match parens
-
-(setq bound-highlights 
-      '(("defn\\s-*\\w+\\s-*[\n]*\\s-*\\[[^]]+\\]" . font-lock-function-name-face)
-	("fn\\s-*[\n]*\\[[^]]+]" . font-lock-function-name-face)
-	(" f \\|(f \\| f)\\|(f)" . font-lock-function-name-face) 
-	(" g \\|(g \\| g)\\|(g)" . font-lock-function-name-face) 
-	(" order \\|(order \\| order)\\|(order)" . font-lock-function-name-face)
-	(" p \\|(p \\| p)\\|(p)" . font-lock-function-name-face)
-	(" p \\|(p \\| p)\\|(p)" . font-lock-function-name-face)
-	("%[0-9]\\|%" . font-lock-keyword-face)
-	("#\\s(" . font-lock-keyword-face)))
+(setq bound-highlights
+      '(("#" . font-lock-function-name-face)
+	("fn->" . font-lock-function-name-face)
+	("%[1-9]" . font-lock-string-face)
+	("%" . font-lock-keyword-face)))
 
 (define-derived-mode bound-mode fundamental-mode "bound"
 "major mode for highlighting bound variables in clojure"
--- examples.clj
+++ examples.clj
@@ -1,22 +1,39 @@
-(def chain 
-  '(fn [f g order]
-     (let [f (nth (iterate add-dim f)  ;; coerce `f` to dimensionality of `g`
-                  (dec (long (count (ffirst g)))))
-           f' (diff-unmixed1 f order 1)
-           g' (diff g order)]
-       (->> order
-            partition-set
-            (map (fn [p]
-                   (mul (multi-compose (nth f' (dec (count p))) g)
-                        (->> p
-                             (map (fn [b] (->> b
-                                              (map-indexed #(*' (long (Math/pow 10 %1)) 
-                                                                %2)) 
-                                              (reduce +')
-                                              (get g'))))
-                             (apply mul)))))
-            (apply add)))))
+(defn chain
+  [f g order]
+  (let [f (nth (iterate add-dim f)
+               (dec (long (count (ffirst g)))))
+        f' (diff-unmixed1 f order 1)
+        g' (diff g order)] 
+    (->> order
+         partition-set
+         (map #(->> %
+                    (map #(->> %
+                               (map-indexed #(*' (long (Math/pow 10 %1)) 
+                                                 %2))
+                               (reduce +')
+                               (get g')))
+                    (apply mul)
+                    (mul (multi-compose (nth f' (dec (count %))) g))))
+         (apply add))))
 
-(defmacro pred   [n] '(fn [f] (fn [x] (((n (fn [g] (fn [h] (h (g f))))) (fn [u] x)) (fn [u] u)))))
-(defmacro pred [n]   '#(((n (%4 (%3 %1))) %2) (%5)))
-(defmacro pred [n] #(#((n #(% #(% %))) %) %))
+(defn chain'
+  [f g order]
+  (let [f (nth (iterate add-dim f)
+               (dec (long (count (ffirst g)))))
+        f' (diff-unmixed1 f order 1)
+        g' (diff g order)]
+    (fn-> order
+          partition-set
+          (map (->> %1
+                    (map (->> %2
+                              (interleave (range))
+                              (partition 2) 
+                              (map (->> %3 ((fn [[idx v]] (*' (long (Math/pow 10 idx)) v))))) 
+                              (reduce +')
+                              (get g')))
+                    (apply mul)
+                    (mul (multi-compose (nth f' (dec (count %1))) g))))
+          (apply add))))
+
+(def pred  #(#(#(% #(% #(% %))) %) %))
+(def pred' #(((%1 (%5 (%4 %2))) %3) (%6)))
