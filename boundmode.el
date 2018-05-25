0ff48954c4fe24fbba7c37f2a5726e490b100f9c
Sophia Gold <sophiagoldnyc@gmail.com>
2018-05-25 09:08:20 -0400

    tricolour

--- boundmode.el
+++ boundmode.el
@@ -1,7 +1,17 @@
 (setq bound-highlights
-      '(("#" . font-lock-function-name-face)
-	("fn->" . font-lock-function-name-face)
+      '(("fn \\[u\\] x" . font-lock-function-name-face)
+       	("fn\\s-*[\n]*\\[[^]]+]*]" . font-lock-function-name-face) 
+	(" p[^a-z]". font-lock-function-name-face)  
+	("[^a-z]h " . font-lock-function-name-face)
+	("[^a-z]n " . font-lock-function-name-face)
+ 	(" b " . font-lock-function-name-face) 
+	("idx" . font-lock-function-name-face)
+	(" v" . font-lock-function-name-face) 
+	("g f" . font-lock-function-name-face) 
+	(" u" . font-lock-function-name-face) 
+	("fn->+" . font-lock-string-face) 
 	("%[1-9]" . font-lock-string-face)
+	("#" . font-lock-keyword-face)
 	("%" . font-lock-keyword-face)))
 
 (define-derived-mode bound-mode fundamental-mode "bound"
--- examples.clj
+++ examples.clj
@@ -1,3 +1,50 @@
+
+
+
+(defn chain
+  [f g order]
+  (let [f (nth (iterate add-dim f)
+               (dec (long (count (ffirst g)))))
+        f' (diff-unmixed1 f order 1)
+        g' (diff g order)] 
+    (->> order
+    	 partition-set
+         (map (fn [p]
+                (mul (multi-compose (nth f' (dec (count p))) g)
+                     (->> p
+                          (map (fn [b] (->> b 
+                                           (map-indexed #(*' (long (Math/pow 10 %1)) 
+                                                             %2))
+                                           (reduce +')
+                                           (get g'))))
+                          (apply mul)))))
+         (apply add))))
+
+
+
+(defn chain
+  [f g order]
+  (let [f (nth (iterate add-dim f)
+               (dec (long (count (ffirst g)))))
+        f' (diff-unmixed1 f order 1)
+        g' (diff g order)
+        x (partition-set order)
+        xf1 #(*' (long (Math/pow 10 %1)) %2)
+        xf2 #(->> %
+                  (reduce +')
+                  (get g'))
+        xf3 #(->> %
+                  (apply mul)
+                  (mul (multi-compose (nth f' (dec (count %))) g)))]
+    (apply add
+           (map
+            (comp xf3
+                  #(map xf2 %)
+                  #(map #(map-indexed xf1 %) %))
+            x))))
+
+
+
 (defn chain
   [f g order]
   (let [f (nth (iterate add-dim f)
@@ -16,24 +63,45 @@
                     (mul (multi-compose (nth f' (dec (count %))) g))))
          (apply add))))
 
-(defn chain'
+
+
+(defn chain
   [f g order]
   (let [f (nth (iterate add-dim f)
                (dec (long (count (ffirst g)))))
         f' (diff-unmixed1 f order 1)
         g' (diff g order)]
-    (fn-> order
-          partition-set
-          (map (->> %1
-                    (map (->> %2
-                              (interleave (range))
-                              (partition 2) 
-                              (map (->> %3 ((fn [[idx v]] (*' (long (Math/pow 10 idx)) v))))) 
-                              (reduce +')
-                              (get g')))
-                    (apply mul)
-                    (mul (multi-compose (nth f' (dec (count %1))) g))))
-          (apply add))))
+    (fn->> order
+           partition-set
+           (map (->> %1
+                     (map (->> %2
+                               (interleave (range))
+                               (partition 2) 
+                               (map (->> %3 ((fn [[idx v]] (*' (long (Math/pow 10 idx)) v)))))
+                               (reduce +')
+                               (get g')))
+                     (apply mul)
+                     (mul (multi-compose (nth f' (dec (count %1))) g))))
+           (apply add))))
 
-(def pred  #(#(#(% #(% #(% %))) %) %))
-(def pred' #(((%1 (%5 (%4 %2))) %3) (%6)))
+
+
+(fn [n]
+  (fn [f]
+    (fn [x]
+      (((n (fn [g]
+             (fn [h]
+	       (h (g f)))))
+	(fn [u] x))
+      (fn [u] u)))))
+
+
+
+#(#(#(% #(% #(%1 %2))) %) %)
+
+
+
+(fn-> (((%1 (%5 (%4 %2))) %3) %6))
+
+
+
